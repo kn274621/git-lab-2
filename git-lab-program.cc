839e6ecd43fa03bcfe769e0a1c3d14227203393f
Karli <kn274621@ohio.edu>
2023-01-24 11:49:15 -0500

    adding program

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Karli Nadsady
+ *        Date: 01\24\23
+ * Description: Lab 2
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
